5be484276ced0d50ee5fbfc7b4683bdfee9973a4
sporge <39319394+rscheppler@users.noreply.github.com>
2022-01-24 14:23:24 -0800

    Sign and font things

---
+++ Assets/Scripts/SignReading.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignReading : MonoBehaviour
+{
+    public GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Text.SetActive(false);
+        }
+    }
+}
