bd065d0026a51fa02d5739b91055d58073b92356
sporge <39319394+rscheppler@users.noreply.github.com>
2022-01-25 18:43:23 -0800

    Jukebox and small enemy fixes

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//make a struct that can be seen in the editor that links a level to a music track
+[System.Serializable]
+    public struct LevelTrack
+    {
+        public string LevelName;
+        public AudioClip Music;
+    };
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    //make sure only one exists
+    public static MusicPlayer Instance;
+    //array of tracks and the levels they go with
+    public LevelTrack[] levelTracks;
+
+    private AudioSource myAud;
+
+    //check that there is only one MusicPlayer
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAud = GetComponent<AudioSource>();
+
+        //play first track
+        FindRightTrack();
+        
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        
+        FindRightTrack();
+    }
+
+    //This should check if the correct music is playing on any given level, and allow levels that share a track to keep playing rather than start and stop
+    private void FindRightTrack()
+    {
+        
+        foreach (LevelTrack LT in levelTracks)
+        {
+            if (SceneManager.GetActiveScene().name == LT.LevelName)
+            {
+                if (myAud.clip == null || myAud.clip.name != LT.Music.name)
+                {
+                    myAud.Stop();
+                    myAud.clip = LT.Music;
+                    myAud.Play();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PatrolEnemy.cs
+++ Assets/Scripts/PatrolEnemy.cs
@@ -7,7 +7,8 @@ public class PatrolEnemy : MonoBehaviour
     public float speed;
     public bool movingRight = false;
 
-    public Transform detection;
+    public Transform frontDetection;
+    public Transform backDetection;
 
     private Rigidbody2D myRB;
 
@@ -37,22 +38,18 @@ public class PatrolEnemy : MonoBehaviour
 
         myRB.AddForce(moveDir * speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(detection.position, Vector2.down, groundRayDist);
-
-        if(groundInfo.collider == false)
+        RaycastHit2D groundInfo = Physics2D.Raycast(frontDetection.position, Vector2.down, groundRayDist);
+        RaycastHit2D backGroundInfo = Physics2D.Raycast(backDetection.position, Vector2.down, groundRayDist);
+        RaycastHit2D wallInfo = Physics2D.Raycast(frontDetection.position, moveDir, wallRayDist);
+        RaycastHit2D backWallInfo = Physics2D.Raycast(backDetection.position, -moveDir, wallRayDist);
+        //make sure that it can keep going a direction or switch, or just stop turning if trapped on both sides
+        //if there wasn't a back check it would spaz if pushed into a strange place
+        if ((groundInfo.collider == false || wallInfo.collider != false) && (backGroundInfo.collider == true && backWallInfo.collider != true))
         {
             movingRight = !movingRight;
             transform.eulerAngles += new Vector3(0, 180, 0);
         }
 
-        
-
-        RaycastHit2D wallInfo = Physics2D.Raycast(detection.position, moveDir, wallRayDist);
-        if (wallInfo.collider != false)
-        {
-            movingRight = !movingRight;
-            transform.eulerAngles += new Vector3(0, 180, 0);
-        }
 
     }
 }
